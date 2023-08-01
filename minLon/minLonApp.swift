b74e0dc72028f603db382d3b955120860e63c209
kikoDevv <103362419+kikoDevv@users.noreply.github.com>
2023-08-01 23:02:50 +0200

    Initial Commit

---
+++ minLon/minLonApp.swift
@@ -0,0 +1,17 @@
+//
+//  minLonApp.swift
+//  minLon
+//
+//  Created by Kiko on 2023-08-01.
+//
+
+import SwiftUI
+
+@main
+struct minLonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
